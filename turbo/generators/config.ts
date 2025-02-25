2d8e2b356dbd80dbdc5dcff412cc2d9b12342e39
Ethan James <ethan.e.james@gmail.com>
2025-02-24 16:12:51 -0800

    move astro template into its own repo

--- turbo/generators/config.ts
+++ turbo/generators/config.ts
@@ -1,9 +1,9 @@
 import { PlopTypes } from "@turbo/gen";
+import { exec } from "node:child_process";
 
 export default function generator(plop: PlopTypes.NodePlopAPI): void {
   plop.setGenerator("add-game", {
-    description:
-      "Add a new Astro project with a folder inside for game files",
+    description: "Add a new Astro project with a folder inside for game files",
     prompts: [
       {
         type: "input",
@@ -12,10 +12,25 @@ export default function generator(plop: PlopTypes.NodePlopAPI): void {
       },
     ],
     actions: [
-      {
-        type: "addMany",
-        destination: "{{ turbo.paths.root }}/apps/{{ dashCase folder }}/",
-        templateFiles: "templates/add-game/**",
+      async function customAction(params) {
+        const { folder } = params as { folder: string };
+        return new Promise((resolve, reject) => {
+          exec(
+            `pnpm create astro@latest ./apps/${folder} -- --template TheMostDangerousGames/zodiac-add-game --install --no-git --skip-houston`,
+            (error, stdout, stderr) => {
+              if (error) {
+                console.error(`Error: ${error.message}`);
+                reject(error);
+              }
+              if (stderr) {
+                console.error(stderr);
+                reject(new Error(stderr));
+              }
+              console.log(stdout);
+              resolve(stdout);
+            },
+          );
+        });
       },
     ],
   });
